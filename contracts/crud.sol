ea40209c9518d65ea60dd94d5750bafffb36fe96
Mr-Dutchman <meelisfidelis@gmail.com>
2022-03-24 10:54:35 +0100

    deplol.py

--- contracts/crud.sol
+++ contracts/crud.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-contract Storage {
+contract Crud {
 	uint256 favoriteNumber;
 	bool favoriteBool;
 
--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,9 +1,35 @@
+#! /usr/bin/python3
 '''deploying contract'''
-from brownie import accounts
+#import os
+from brownie import Crud, accounts #,config
+
+
 def deploy_crud():
     '''deploy function'''
+    #using ganache
     account = accounts[0]
-    print(account)
+
+    #using brownie to password encrypt the key
+    #account = accounts.load("KEY")
+
+    #using .env file
+    #account =accounts.add(os.getenv("KEY"))
+
+    #using special .yaml config
+    #account = accounts.add(config['wallets']['from_key'])
+    print('Deploying contract...')
+    crud = Crud.deploy({
+        'from': account
+        })
+    print('Getting the stores valus...')
+    store_value = crud.retrieve()
+    print(store_value)
+
+
+    transaction = crud.store(15, {'from':account})
+    transaction.wait(1)
+    updated_crud = crud.retrieve()
+    print(updated_crud)
 
 def main():
     '''calling the deploy function'''
